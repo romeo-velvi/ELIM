b6c0f22a7ddbb9e1a5e5230d7f43b19dd4e9720a
velvi-stud <romeo.velvi001@studenti.uniparthenope.it>
2022-03-04 12:17:18 +0100

    canny

--- 1 Canny/exe.cpp
+++ 1 Canny/exe.cpp
@@ -1,13 +1,6 @@
-#include "opencv2/core.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/videoio.hpp"
+#include "opencv2/opencv.hpp"
 #include <iostream>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <time.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 using namespace std;
